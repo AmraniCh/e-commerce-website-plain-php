5867d2069c6a9c06b071a648d8bba601ff6bf75b
djkml001 <djkml001@gmail.com>
2019-06-07 02:40:10 +0000

    add email

--- public-includes/functions.php
+++ public-includes/functions.php
@@ -24,8 +24,8 @@ function sendEmail($RecipientEmail,$Nom)
         $mail->isSMTP();                                            // Set mailer to use SMTP
         $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'elamrani.sv.laza@gmail.com';                     // SMTP username
-        $mail->Password   = '';                               // SMTP password
+        $mail->Username   = 'mgaservicegroup@gmail.com';                     // SMTP username
+        $mail->Password   = '123456789Kamal';                               // SMTP password
         $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
         $mail->Port       = 587;                                    // TCP port to connect to
 
